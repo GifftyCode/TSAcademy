567f1c472fe1b70b6735b901fe83840cedfd97b0
GifftyCode <ulokangozi@yahoo.com>
2026-07-22 04:29:12 +0100

    updating a student info

--- assignment1-RestAPI/app.js
+++ assignment1-RestAPI/app.js
@@ -62,6 +62,19 @@ app.get("/api/students/:id", (req, res) => {
   res.status(200).json(student);
 });
 
+// Update student information
+app.patch("/api/students/:id", (req, res) => {
+  const { id } = req.params;
+
+  const student = students.find((student) => student.studentId === id);
+
+  if (!student) return res.status(404).json({ error: "Student not found!" });
+
+  Object.assign(student, req.body);
+
+  res.status(201).json(student);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
