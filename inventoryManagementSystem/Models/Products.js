66c5a2cb7e2ad4e73ea7aee760213f3d10ed9693
GifftyCode <ulokangozi@yahoo.com>
2026-08-03 04:47:59 +0100

    user/product exports

--- inventoryManagementSystem/Models/Products.js
+++ inventoryManagementSystem/Models/Products.js
@@ -1,26 +1,32 @@
-const mongose = require("mongose");
+const mongoose = require("mongoose");
 const { timeStamp } = require("node:console");
 
-const productSchema = new mongose.Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-  size: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  timeStamp: true,
-});
+  { timeStamp: true },
+);
+
+// create model from schema
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
--- inventoryManagementSystem/Models/Users.js
+++ inventoryManagementSystem/Models/Users.js
@@ -3,35 +3,38 @@ const bcrypt = require("bcryptjs");
 const { type } = require("node:os");
 const { timeStamp } = require("node:console");
 
-const userSchema = new mongose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  timeStamp: true, // Date created and date updated
-});
+  { timeStamp: true }, // Date created and date updated
+);
 
 // Create Model from schema
-const User = mongose.model("user", userSchema);
+const User = mongoose.model("user", userSchema);
+module.exports = User;
