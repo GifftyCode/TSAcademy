7a4bd9eba9a361e9104aa13dce08b528901b7e89
GifftyCode <ulokangozi@yahoo.com>
2026-08-11 09:49:55 +0100

    user login controller

--- inventoryManagementSystem/Controllers/UserController.js
+++ inventoryManagementSystem/Controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../Models/Users");
+const jwt = require("jsonwebtoken");
 
 // Create a user
 exports.createUser = async (req, res) => {
@@ -49,5 +50,35 @@ exports.createUser = async (req, res) => {
     res
       .status(500)
       .json({ message: "Error creating user", error: error.message });
+  }
+};
+
+// Log in User
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if all required fields are provided
+    if (!email || !password)
+      return res.status(400).json({ message: "Provide all required fields" });
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Check if password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid password" });
+
+    // Generate a token
+    const token = jwt.sign(
+      { id: user._id, email: user.email, name: user.name },
+      process.env.JWT_SECRET,
+    );
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ message: "Error logging in", error: error.message });
   }
 };
