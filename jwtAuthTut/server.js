9303538499a44fe55358fb4ec4bee95f03944dd8
GifftyCode <ulokangozi@yahoo.com>
2026-08-14 05:43:48 +0100

    created access token

---
+++ jwtAuthTut/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(express.json());
+
+const posts = [
+  {
+    username: "Giffty",
+    title: "Post 1",
+  },
+  {
+    username: "Sammy",
+    title: "Post 2",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.post("/login", (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken });
+});
+
+app.listen(3000);
