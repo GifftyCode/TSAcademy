3108bf61bb3186393223e98f393f64a9e534b3d7
GifftyCode <ulokangozi@yahoo.com>
2026-08-24 15:49:25 +0100

    send mail features

---
+++ inventoryManagementSystem/Middleware/sendEmail.js
@@ -0,0 +1,18 @@
+const transporter = require("../Config/email");
+
+const sendEmail = async (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("Error sending email.", error);
+  }
+};
+
+module.exports = sendEmail;
