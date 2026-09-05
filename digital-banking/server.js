6d7e8ba3bb9bbb3f4266109ec3f985cc475ec2f7
GifftyCode <ulokangozi@yahoo.com>
2026-09-05 15:34:29 +0100

    Complete digital banking backend assignment

--- digital-banking/server.js
+++ digital-banking/server.js
@@ -1,23 +1,26 @@
+// server.js
+
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
-
-const onboardRoutes = require("./routes/onboardRoutes");
-const accountRoutes = require("./routes/accountRoutes");
-const bankingRoutes = require("./routes/bankingRoutes");
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
-app.use("/api", onboardRoutes);
-app.use("/api", accountRoutes);
-app.use("/api", bankingRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Digital Banking API is alive and well" });
 });
+
+const onboardRoutes = require("./routes/onboardRoutes");
+const accountRoutes = require("./routes/accountRoutes");
+const bankingRoutes = require("./routes/bankingRoutes");
+
+app.use("/api", onboardRoutes);
+app.use("/api", accountRoutes);
+app.use("/api", bankingRoutes);
 
 const PORT = process.env.PORT || 3000;
 
